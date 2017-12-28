8fcdaa18b8bb22bd5273b2172dfac0c8d5644bb0
Seve Sheldon <sevesheldon@gmail.com>
2017-12-28 14:03:38 -0800

    Assign bootstrap to divs to give the first row it's initial shape.

--- wp-content/themes/SDsincere/template-menu-page.php
+++ wp-content/themes/SDsincere/template-menu-page.php
@@ -9,11 +9,11 @@
 			<!-- article -->
 			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-				<div class="page-content">
+				<div class="page-content container">
 
 					<?php the_content(); ?>
 
-					<div id="indica-container">	
+					<div id="indica-rows" class="row">	
 
 					<?php if( have_rows('indica') ): ?>
 							<?php while( have_rows('indica') ): the_row(); 
@@ -22,29 +22,45 @@
 									$image_url = $image_object['sizes'][$image_size];
 									?>
 
-									<div class="indica-content">
+									<div class="col-sm-12" id="indica-content">
+
+										<div class="row">
+
+											<div class="col-sm-3">
 			    		    			
-			    		    			<img src="<?php echo $image_url; ?>" />
+					    		    			<img src="<?php echo $image_url; ?>" />
+
+					    		    		</div>						    		    						 
+
+											<div class="col-sm-6">	
 			    		    				
-			    		    			<h2><?php the_sub_field('name-indica'); ?></h2>
+					    		    			<h2><?php the_sub_field('name-indica'); ?></h2>
 
-			    		    			<p>Indica | <?php the_sub_field('description-indica'); ?></p>
+					    		    			<p>Indica | <?php the_sub_field('description-indica'); ?></p>
 
-			    		    			<p>THC<br><?php the_sub_field('thc-indica'); ?>%</p>
+					    		    			<p>THC<br><?php the_sub_field('thc-indica'); ?>%</p>
 
-			    		    			<p>CBD<br><?php the_sub_field('cbd-indica'); ?>%</p>
+					    		    			<p>CBD<br><?php the_sub_field('cbd-indica'); ?>%</p>
 
-			    		    			<p>CBN<br><?php the_sub_field('cbn-indica'); ?>%</p>
+					    		    			<p>CBN<br><?php the_sub_field('cbn-indica'); ?>%</p>
 
-			    		    			<div id="1/8-indica"><p>1/8</p><h4>$<?php the_sub_field('1/8-indica'); ?></h4></div>
+					    		    		</div>		
+					    		    	
 
-			    		    			<div id="1/4-indica"><p>1/4</p><h4>$<?php the_sub_field('1/4-indica'); ?></h4></div>
+											<div class="col-sm-3">	
 
-			    		    			<div id="1/2-indica"><p>1/2</p><h4>$<?php the_sub_field('1/2-indica'); ?></h4></div>
+					    		    			<div id="1/8-indica"><p>1/8</p><h4>$<?php the_sub_field('1/8-indica'); ?></h4></div>
 
-			    		    			<div id="oz-indica"><p>Oz.</p><h4>$<?php the_sub_field('oz-indica'); ?></h4></div>
+					    		    			<div id="1/4-indica"><p>1/4</p><h4>$<?php the_sub_field('1/4-indica'); ?></h4></div>
 
+					    		    			<div id="1/2-indica"><p>1/2</p><h4>$<?php the_sub_field('1/2-indica'); ?></h4></div>
 
+					    		    			<div id="oz-indica"><p>Oz.</p><h4>$<?php the_sub_field('oz-indica'); ?></h4></div>
+
+					    		    		</div>
+
+					    		    	</div>	
+					    		    		
 				    		    	</div>
 			    		    		
 							<?php endwhile; ?>
