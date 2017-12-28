bed9812a983a953865c49eb6a49f454bd421913a
Seve Sheldon <sevesheldon@gmail.com>
2017-12-28 13:49:29 -0800

    Add the corresponding repeater fields (ACF) for Indica to the Menu Page template.

--- wp-content/themes/SDsincere/template-menu-page.php
+++ wp-content/themes/SDsincere/template-menu-page.php
@@ -9,7 +9,50 @@
 			<!-- article -->
 			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-				<?php the_content(); ?>
+				<div class="page-content">
+
+					<?php the_content(); ?>
+
+					<div id="indica-container">	
+
+					<?php if( have_rows('indica') ): ?>
+							<?php while( have_rows('indica') ): the_row(); 
+									$image_object = get_sub_field('image-indica');
+									$image_size = 'thumbnail';
+									$image_url = $image_object['sizes'][$image_size];
+									?>
+
+									<div class="indica-content">
+			    		    			
+			    		    			<img src="<?php echo $image_url; ?>" />
+			    		    				
+			    		    			<h2><?php the_sub_field('name-indica'); ?></h2>
+
+			    		    			<p>Indica | <?php the_sub_field('description-indica'); ?></p>
+
+			    		    			<p>THC<br><?php the_sub_field('thc-indica'); ?>%</p>
+
+			    		    			<p>CBD<br><?php the_sub_field('cbd-indica'); ?>%</p>
+
+			    		    			<p>CBN<br><?php the_sub_field('cbn-indica'); ?>%</p>
+
+			    		    			<div id="1/8-indica"><p>1/8</p><h4>$<?php the_sub_field('1/8-indica'); ?></h4></div>
+
+			    		    			<div id="1/4-indica"><p>1/4</p><h4>$<?php the_sub_field('1/4-indica'); ?></h4></div>
+
+			    		    			<div id="1/2-indica"><p>1/2</p><h4>$<?php the_sub_field('1/2-indica'); ?></h4></div>
+
+			    		    			<div id="oz-indica"><p>Oz.</p><h4>$<?php the_sub_field('oz-indica'); ?></h4></div>
+
+
+				    		    	</div>
+			    		    		
+							<?php endwhile; ?>
+					<?php endif; ?>
+
+					</div>
+
+				</div>	
 
 				<br class="clear">
 
