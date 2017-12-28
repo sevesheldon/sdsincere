a3a2d861e86057054810709388d49ae7eb4bea0e
Seve Sheldon <sevesheldon@gmail.com>
2017-12-28 14:14:12 -0800

    Assign more bootstrap to now second level of rows.

--- wp-content/themes/SDsincere/template-menu-page.php
+++ wp-content/themes/SDsincere/template-menu-page.php
@@ -32,30 +32,70 @@
 
 					    		    		</div>						    		    						 
 
-											<div class="col-sm-6">	
+											<div class="col-sm-6">
+
+												<div class="row">
+
+													<div class="col-sm-6">	
 			    		    				
-					    		    			<h2><?php the_sub_field('name-indica'); ?></h2>
+					    		    					<h2><?php the_sub_field('name-indica'); ?></h2>
 
-					    		    			<p>Indica | <?php the_sub_field('description-indica'); ?></p>
+					    		    					<p>Indica | <?php the_sub_field('description-indica'); ?></p>
 
-					    		    			<p>THC<br><?php the_sub_field('thc-indica'); ?>%</p>
+					    		    				</div>
+					    		    				
+					    		    				<div class="col-sm-2">	
 
-					    		    			<p>CBD<br><?php the_sub_field('cbd-indica'); ?>%</p>
+					    		    					<p>THC<br><?php the_sub_field('thc-indica'); ?>%</p>
 
-					    		    			<p>CBN<br><?php the_sub_field('cbn-indica'); ?>%</p>
+					    		    				</div>	
 
-					    		    		</div>		
+					    		    				<div class="col-sm-2">
+
+					    		    					<p>CBD<br><?php the_sub_field('cbd-indica'); ?>%</p>
+
+					    		    				</div>
+
+					    		    				<div class="col-sm-2">
+
+					    		    					<p>CBN<br><?php the_sub_field('cbn-indica'); ?>%</p>
+
+					    		    				</div>	
+
+					    		    			</div>
+
+					    		    		</div>			
 					    		    	
 
-											<div class="col-sm-3">	
+											<div class="col-sm-3">
 
-					    		    			<div id="1/8-indica"><p>1/8</p><h4>$<?php the_sub_field('1/8-indica'); ?></h4></div>
+												<div class="row">
 
-					    		    			<div id="1/4-indica"><p>1/4</p><h4>$<?php the_sub_field('1/4-indica'); ?></h4></div>
+													<div class="col-sm-3">	
 
-					    		    			<div id="1/2-indica"><p>1/2</p><h4>$<?php the_sub_field('1/2-indica'); ?></h4></div>
+							    		    			<div id="1/8-indica"><p>1/8</p><h4>$<?php the_sub_field('1/8-indica'); ?></h4></div>
 
-					    		    			<div id="oz-indica"><p>Oz.</p><h4>$<?php the_sub_field('oz-indica'); ?></h4></div>
+							    		    		</div>
+							    		    		
+							    		    		<div class="col-sm-3">		
+
+							    		    			<div id="1/4-indica"><p>1/4</p><h4>$<?php the_sub_field('1/4-indica'); ?></h4></div>
+
+							    		    		</div>
+							    		    		
+							    		    		<div class="col-sm-3">		
+
+							    		    			<div id="1/2-indica"><p>1/2</p><h4>$<?php the_sub_field('1/2-indica'); ?></h4></div>
+
+							    		    		</div>
+							    		    		
+							    		    		<div class="col-sm-3">		
+
+							    		    			<div id="oz-indica"><p>Oz.</p><h4>$<?php the_sub_field('oz-indica'); ?></h4></div>
+
+							    		    		</div>	
+
+						    		    		</div>		
 
 					    		    		</div>
 
