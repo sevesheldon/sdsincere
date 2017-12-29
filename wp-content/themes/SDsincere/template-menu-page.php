9f92170bf57b4ec0cbf3de06cca5e6543fb2bcf0
Seve Sheldon <sevesheldon@gmail.com>
2017-12-29 10:04:07 -0800

    Add code to menu-page template to now include ACF content for sativa category, add extra top margin to #cat-name-sativa H1.

--- wp-content/themes/SDsincere/template-menu-page.php
+++ wp-content/themes/SDsincere/template-menu-page.php
@@ -15,7 +15,7 @@
 
 					<div id="indica-rows" class="row">
 
-						<div class="cat-name">
+						<div class="cat-name" id="cat-name-indica">
 
 							<h1>Indica</h1>
 
@@ -104,6 +104,113 @@
 							    		    		<div class="col-sm-3">		
 
 							    		    			<div class="weight" id="oz-indica"><p>Oz.</p><h4 class="values price">$<?php the_sub_field('oz-indica'); ?></h4></div>
+
+							    		    		</div>	
+
+						    		    		</div>		
+
+					    		    		</div>
+
+					    		    	</div>	
+					    		    		
+				    		    	</div>
+			    		    		
+							<?php endwhile; ?>
+					<?php endif; ?>
+
+					</div>
+
+					<div id="sativa-rows" class="row">
+
+						<div class="cat-name" id="cat-name-sativa">
+
+							<h1>Sativa</h1>
+
+						</div>		
+
+					<?php if( have_rows('sativa') ): ?>
+							<?php while( have_rows('sativa') ): the_row(); 
+									$image_object = get_sub_field('image-sativa');
+									$image_size = 'thumbnail';
+									$image_url = $image_object['sizes'][$image_size];
+									?>
+
+									<div class="content col-sm-12" id="sativa-content">
+
+										<div class="row">
+
+											<div class="row1 col-1 col-sm-2">
+			    		    			
+					    		    			<img src="<?php echo $image_url; ?>" />
+
+					    		    		</div>						    		    						 
+
+											<div class="row1 col-2 col-sm-7">
+
+												<div class="row">
+
+													<div class="col-sm-9">	
+			    		    				
+					    		    					<h2><?php the_sub_field('name-sativa'); ?></h2>
+
+					    		    					<p>Sativa | <?php the_sub_field('description-sativa'); ?></p>
+
+					    		    				</div>
+					    		    				
+					    		    				<div class="col-sm-1">	
+
+					    		    					<p>THC<p/>
+
+					    		    					<p class="values"><?php the_sub_field('thc-sativa'); ?>%</p>
+
+					    		    				</div>	
+
+					    		    				<div class="col-sm-1">
+
+					    		    					<p>CBD<p/>
+
+					    		    					<p class="values"><?php the_sub_field('cbd-sativa'); ?>%</p>
+
+					    		    				</div>
+
+					    		    				<div class="col-sm-1">
+
+					    		    					<p>CBN<p/>
+
+					    		    					<p class="values"><?php the_sub_field('cbn-sativa'); ?>%</p>
+
+					    		    				</div>	
+
+					    		    			</div>
+
+					    		    		</div>			
+					    		    	
+
+											<div class="row1 col-3 col-sm-3">
+
+												<div class="prices row">
+
+													<div class="col-sm-3">	
+
+							    		    			<div class="weight" id="1/8-sativa"><p>1/8</p><h4 class="values price">$<?php the_sub_field('1/8-sativa'); ?></h4></div>
+
+							    		    		</div>
+							    		    		
+							    		    		<div class="col-sm-3">		
+
+							    		    			<div class="weight" id="1/4-sativa"><p>1/4</p><h4 class="values price">$<?php the_sub_field('1/4-sativa'); ?></h4></div>
+
+							    		    		</div>
+							    		    		
+							    		    		<div class="col-sm-3">		
+
+							    		    			<div class="weight" id="1/2-sativa"><p>1/2</p><h4 class="values price">$<?php the_sub_field('1/2-sativa'); ?></h4></div>
+
+							    		    		</div>
+							    		    		
+							    		    		<div class="col-sm-3">		
+
+							    		    			<div class="weight" id="oz-sativa"><p>Oz.</p><h4 class="values price">$<?php the_sub_field('oz-sativa'); ?></h4></div>
 
 							    		    		</div>	
 
