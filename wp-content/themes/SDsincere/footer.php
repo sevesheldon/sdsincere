6bff67d8725b746d5302e5d81a6038cab23526c7
Seve Sheldon <sevesheldon@gmail.com>
2018-01-03 12:56:54 -0800

    Remove 'Copyright' from  footer.

--- wp-content/themes/SDsincere/footer.php
+++ wp-content/themes/SDsincere/footer.php
@@ -3,7 +3,7 @@
 
 				<!-- copyright -->
 				<p class="copyright">
-					&copy; <?php echo date('Y'); ?> Copyright <?php bloginfo('name'); ?> |
+					&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?> |
 					<a href="http://kentoncodeworks.com/" title="KCW">Kenton Codeworks</a>
 				</p>
 				<!-- /copyright -->
