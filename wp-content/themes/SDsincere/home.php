72371d08415b3b08694f77a2078aab9b0df2f564
Seve Sheldon <sevesheldon@gmail.com>
2018-01-09 11:30:25 -0800

    Add sidebar search bar to home and category, centered it right above footer.

--- wp-content/themes/SDsincere/home.php
+++ wp-content/themes/SDsincere/home.php
@@ -209,10 +209,8 @@ wp_reset_postdata();
 	</section>
 	<!-- /section -->
 
-<!-- 		<div class="extra-nav" role="navigation">
-			<h5>Scroll to:</h5>
-			<?php wp_nav_menu('extra-menu'); ?>
-		</div> -->
 	</main>
+
+<?php get_sidebar(); ?>	
 
 <?php get_footer(); ?>
